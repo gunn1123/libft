7b311579c295f226c7e56f89c120e7ef2cc98e5e
Chanat <chanat.gunn@outlook.co.th>
2022-03-28 14:57:15 +0700

    add ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -1,39 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cthamsur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/27 21:31:35 by cthamsur          #+#    #+#             */
-/*   Updated: 2022/03/27 21:40:41 by cthamsur         ###   ########.fr       */
+/*   Created: 2022/03/27 20:39:03 by cthamsur          #+#    #+#             */
+/*   Updated: 2022/03/27 20:44:19 by cthamsur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 
-void	*ft_memmove(void *str1, const void *str2, size_t n)
+void    *ft_memset(void *block, int c, size_t size)
 {
-	char					*head_str1;
-	const unsigned char		*ptr_const;
-	size_t					i;	
-
-	i = 0;
-	head_str1 = str1;
-	ptr_const = str2;
-	if (!str1 && !str2)
-		return (str1);
-	if (str1 < str2)
-	{
-		while (i++ < n)
-			*head_str1++ = *ptr_const++;
-	}
-	else
-	{
-		head_str1 += n;
-		ptr_const += n;
-		while (n-- != 0)
-			*--head_str1 = *--ptr_const;
-	}
-	return (str1);
+    size_t  i;
+    unsigned char   *head;
+    
+    i = 0;
+    head = block;
+    while (i < size)
+    {
+        *head++ = (unsigned char) c;
+        i++;
+    }
+    return (block);
 }
