d6d469b2b13e1f510132446c813f7a8b267794e3
Chanat Thamsuriya <cthamsur@z3t1c4.42bangkok.com>
2022-04-04 14:08:27 +0700

    done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,31 @@
 /*   By: cthamsur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 20:39:03 by cthamsur          #+#    #+#             */
-/*   Updated: 2022/03/27 20:44:19 by cthamsur         ###   ########.fr       */
+/*   Updated: 2022/04/04 14:01:26 by cthamsur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 int	ft_atoi(const char *str)
 {	
-	int	result;
-	int	sign;
+	int	n;
+	int	s;
 
-	sign = 1;
-	result = 0;
+	s = 1;
+	n = 0;
 	while ((*str >= '\t' && *str <= '\r') || *str == ' ')
 		++str;
 	if (*str == '+' || *str == '-')
 	{
 		if (*(str++) == '-')
-			sign *= -1;
+			s *= -1;
+		str++;
 	}
 	while (ft_isdigit(*str))
 	{
-		result = result * 10;
-		result += (*str++ - '0');
+		n *= 10;
+		n += (*str++ - '0');
+		str++;
 	}
-	return (sign * result);
+	return (n * s);
 }
