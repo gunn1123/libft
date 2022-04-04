c216747de8ec3bd04a53af80a6c9330819b9723a
Chanat Thamsuriya <cthamsur@z3t1c4.42bangkok.com>
2022-04-04 13:40:15 +0700

    fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cthamsur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 19:02:43 by cthamsur          #+#    #+#             */
-/*   Updated: 2022/04/04 13:27:34 by cthamsur         ###   ########.fr       */
+/*   Updated: 2022/04/04 13:29:56 by cthamsur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	j;
 
 	i = ft_strlen(dst);
-	if (i > dstsize || dstsize == 0)
-		return (dstsize + ft_strlen(src));
+	if (i > size || size == 0)
+		return (size + ft_strlen(src));
 	j = 0;
-	while ((i + j +1) < dstsize && src[j])
+	while ((i + j +1) < size && src[j])
 	{
 		dst[i + j] = src[j];
 		j++;
